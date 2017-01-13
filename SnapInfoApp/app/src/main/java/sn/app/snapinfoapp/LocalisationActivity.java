e2de308aab09a664897eedec6a0e625afc5fae15
beaukams <kamstelecom@hotmail.com>
2017-01-13 01:18:41 +0000

    mobile

--- SnapInfoApp/app/src/main/java/sn/app/snapinfoapp/LocalisationActivity.java
+++ SnapInfoApp/app/src/main/java/sn/app/snapinfoapp/LocalisationActivity.java
@@ -26,72 +26,17 @@ import java.net.URLEncoder;
 import java.util.Iterator;
 import java.util.List;
 
-public class LocalisationActivity extends AppCompatActivity implements LocationListener {
+public class LocalisationActivity extends AppCompatActivity {
 
-    private double latitude;
-    private double longitude;
-    private String typeStructure;
-    private String telephone = "";
-    private String CellID = "";
-    private String MNC = "";
-    private String MCC = "";
-    private String LAC = "";
-    private String operateur = "";
-    private String IMEI = "";
-
-    private LocationManager locationManager;
-    private TelephonyManager telephonyManager;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localisation);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        telephonyManager= (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                finish();
-            }
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0,
-                    this);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0,
-                    this);
-        }
 
 
     }
 
-    public void updateLocalisation(){
-        this.MCC = telephonyManager.getSimCountryIso();
-        this.MNC = telephonyManager.getNetworkOperator()+ "--"+telephonyManager.getCellLocation().toString();
-        this.operateur = telephonyManager.getNetworkOperatorName()+"-"+telephonyManager.getSimOperator().;
-        this.IMEI = telephonyManager.getDeviceId();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            this.telephone = ".."+telephonyManager.getPhoneCount() ;//.getPhoneCount();
-        }else{
-            this.telephone = telephonyManager.getSubscriberId();
-        }
 
-        TextView valMCC= (TextView) findViewById(R.id.valMCC);
-        valMCC.setText(String.valueOf(this.MCC));
-
-        TextView valMNC= (TextView) findViewById(R.id.valMNC);
-        valMNC.setText(String.valueOf(this.MNC));
-
-        TextView valTel= (TextView) findViewById(R.id.valTel);
-        valTel.setText(String.valueOf(this.telephone));
-
-        TextView valOperateur= (TextView) findViewById(R.id.valOperateur);
-        valOperateur.setText(String.valueOf(this.operateur));
-
-         /*
-        TextView valLng= (TextView) findViewById(R.id.valLng);
-        valLng.setText(String.valueOf(this.longitude));*/
-
-    }
 
     public void getPositionGPS() {
         //selectionner tous les fournisseurs
@@ -125,8 +70,6 @@ public class LocalisationActivity extends AppCompatActivity implements LocationL
 
     }
 
-
-
     /**
      * deserialiser le menu
      */
@@ -150,61 +93,4 @@ public class LocalisationActivity extends AppCompatActivity implements LocationL
     }
 
 
-    public JSONObject formule() throws JSONException {
-        JSONObject postDataParams = new JSONObject();
-        postDataParams.put("latitude", this.latitude);
-        postDataParams.put("longitude", this.longitude);
-        postDataParams.put("typeStructure", this.typeStructure);
-        postDataParams.put("MNC", this.MNC);
-        postDataParams.put("LAC", this.LAC);
-        postDataParams.put("MCC", this.MCC);
-        postDataParams.put("CellID", this.CellID);
-        postDataParams.put("telephone", this.telephone);
-        postDataParams.put("operateur", this.operateur);
-        return postDataParams;
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        this.longitude = location.getLongitude();
-        this.latitude = location.getLatitude();
-        TextView valLat= (TextView) findViewById(R.id.valLat);
-        valLat.setText(String.valueOf(this.latitude));
-
-        TextView valLng= (TextView) findViewById(R.id.valLng);
-        valLng.setText(String.valueOf(this.longitude));
-
-        Toast.makeText(this, "Nouvelle localisation", Toast.LENGTH_LONG).show();
-
-        updateLocalisation();
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        String newStatus = "";
-        switch (status) {
-            case LocationProvider.OUT_OF_SERVICE:
-                newStatus = "OUT_OF_SERVICE";
-                break;
-            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                newStatus = "TEMPORARILY_UNAVAILABLE";
-                break;
-            case LocationProvider.AVAILABLE:
-                newStatus = "AVAILABLE";
-                break;
-        }
-
-        //Toast.makeText(this, "statut change", Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-        Toast.makeText(this, "La localisation est disponible", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-        Toast.makeText(this, "La localisation GPS est désactivée", Toast.LENGTH_SHORT).show();
-    }
 }
