7474f83a05ec1630ad6a935a3c396bf7744a30c6
philippegui2 <philippekolama@gmail.com>
2017-02-04 10:49:20 +0000

    revue recup REST

--- API/snapweb.php
+++ API/snapweb.php
@@ -1,8 +1,8 @@
 <?php
 //les données reçues --- photo-latitude-longitude-commentaire-libelle-idStructure
     //Connexion à la base de données  
-        //include_once("../server/baseConf.php");//connexion base serveur
-        //include_once("../server/requetes.php");//lien requetes base serveur
+        include_once("../server/baseConf.php");//connexion base serveur
+        include_once("../server/requetes.php");//lien requetes base serveur
         //----- Vers client -------
         include_once("../web/database/baseConf.php");//connexion base client
         include_once("../web/database/Requetes.class.php");//lien requetes base client
@@ -16,13 +16,12 @@
         
 try {
 
-	switch ($_SERVER['REQUEST_METHOD']) {
-            case 'POST':        
-                $requete->putDonnees($_REQUEST("idStructure"),$_REQUEST("photo"),$_REQUEST("longitude"),$_REQUEST("longitude"),$_REQUEST("latitude"),$_REQUEST("lieu"),$_REQUEST("datePhoto"),$_REQUEST("commentaire"));
-                break;
-            default:
-                echo "rien!!!";
-                break;
+	if($_SERVER['REQUEST_METHOD']=="POST") {
+
+        echo $_REQUEST["idStructure"]." ".$_REQUEST["photo"]." ".$_REQUEST["longitude"]." ".$_REQUEST["latitude"]." ".$_REQUEST["lieu"]." ".$_REQUEST["datePhoto"]." ".$_REQUEST["commentaire"];
+
+        $requete->putDonnees($_REQUEST["idStructure"],$_REQUEST["photo"],$_REQUEST["longitude"],$_REQUEST["latitude"],$_REQUEST["lieu"],$_REQUEST["datePhoto"],$_REQUEST["commentaire"]);
+
 
         }
     }
---
+++ web/teste.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="POST" action="../API/snapweb.php">
+            <input type="text" name="idStructure" value="2"/>
+            <input type="text" name="photo" value="photo"/>
+            <input type="text" name="longitude" value="-17.23452"/>
+            <input type="text" name="latitude" value="14.1345"/>
+            <input type="text" name="lieu"value="lieu"/>
+            <input type="text" name="datePhoto" value="2017-02-08 00:00:00"/>
+            <input type="text" name="commentaire" value="commentaire"/>
+            <input type="submit" value="envoyer">
+        </form>
+    </body>
+</html>
